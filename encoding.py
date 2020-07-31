99a65a55cc898c453223d593b4b6560eef91301f
lee-man <minli.amoy@gmail.com>
2020-07-31 16:39:31 +0800

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -162,8 +162,8 @@ class TwoDimEncoding(object):
                     id_list = np.random.choice(id_list, size=self.num_id, replace=False, p=self.sc_counts)
                     self.group_mapping[j] = {str(id): i for (i, id) in enumerate(id_list)}
                     for (key, value) in self.group_mapping[j]:
-                        group_id = value // self.chain_ctrl
-                        sc_conf[j-1][int(key)] = group_id
+                        group_id = key // self.chain_ctrl
+                        sc_conf[j-1][int(value)] = group_id
                     for conf_ind in range(j-1):
                         print((sc_conf[j-1] == sc_conf[conf_ind]).shape)
                         exit()
